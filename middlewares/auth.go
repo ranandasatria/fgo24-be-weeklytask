3344c77f6cbf23c09816076ed6bbe11af823dfd6
ranandasatria <rananda24@gmail.com>
2025-06-30 02:02:59 +0700

    feat: add transfer history and search endpoint

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,50 +1,48 @@
 package middlewares
 
 import (
+	"ewallet_be/utils"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 )
 
 func VerifyToken() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		authHeader := ctx.GetHeader("Authorization")
-		if authHeader == "" {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Missing Authorization header"})
-			ctx.Abort()
+		godotenv.Load()
+		secretKey := os.Getenv("APP_SECRET")
+		token := strings.Split(ctx.GetHeader("Authorization"), "Bearer ")
+
+		if len(token) < 2 {
+			ctx.JSON(http.StatusUnauthorized, utils.Response{
+				Success: false,
+				Message: "Unauthorized",
+			})
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
-
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("APP_SECRET")), nil
+		rawToken, err := jwt.Parse(token[1], func(t *jwt.Token) (interface{}, error) {
+			return []byte(secretKey), nil
 		})
 
-		if err != nil || !token.Valid {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
-			ctx.Abort()
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, utils.Response{
+				Success: false,
+				Message: "Invalid token",
+			})
+			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
 
-		claims, ok := token.Claims.(jwt.MapClaims)
-		if !ok {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid token claims"})
-			ctx.Abort()
-			return
-		}
+		userIdFloat := rawToken.Claims.(jwt.MapClaims)["userId"].(float64)
+		userId := int(userIdFloat)
 
-		userIdFloat, ok := claims["userId"].(float64)
-		if !ok {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"message": "userId not found in token"})
-			ctx.Abort()
-			return
-		}
-
-		ctx.Set("userId", int(userIdFloat))
+		ctx.Set("userId", userId)
 		ctx.Next()
+
 	}
 }
